5ef141c26cb5eb1ef3a6579f1cb087e3c561be6a
matin ahmadi <matinahmadi.programmer@gmail.com>
2025-04-27 19:18:34 +0430

    add position in pieces

--- chess_engine/engine/piece/abstract.py
+++ chess_engine/engine/piece/abstract.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 from .color import PieceColor
+from .position import PiecePosition
 
 
 class Piece(ABC):
-    def __init__(self, color: PieceColor) -> None:
+    def __init__(self, color: PieceColor, position: PiecePosition) -> None:
         self.color = color
+        self.position = position
 
     @abstractmethod
     def get_moves(self):
@@ -15,7 +17,7 @@ class Piece(ABC):
         pass
 
     def __str__(self) -> str:
-        return f"{self.__class__.__name__}-{self.color}"
+        return f"{self.__class__.__name__}-{self.color}-{self.position}"
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} {self.color=}>"
+        return f"{self.__class__.__name__}({self.color=}, {self.position=})"
