ce2a36d6b14bc92b6805f0b98ede1a8547116f27
Matin Ahmadi <matinahmadi.programmer@gmail.com>
2025-07-10 11:00:08 +0330

    chore(chess_engine): add alowed host to get data from django with axios

--- chess_engine/api/main.py
+++ chess_engine/api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from chess_engine.engine.game import Game
 
 app = FastAPI()
 game_instance = Game()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def get_game_status():
